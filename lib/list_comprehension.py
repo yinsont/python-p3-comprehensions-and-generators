b520b9b5fa2c2c0f877c39faa6a5b62e3fc0021d
Yinson Tso <yinson.tso@gmail.com>
2023-05-01 10:56:18 -0400

    comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    remove_odd = [n for n in num_list if n%2 == 0]
+    return(remove_odd)
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation = [f'{n}!' for n in sentence_list]
+    return add_exclamation
